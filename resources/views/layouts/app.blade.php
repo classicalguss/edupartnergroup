dbac746054346a504c8a897d0073f864042c7515
Ghassan Barghouti <ghassan.barghouti@maf.co.ae>
2020-06-15 23:03:34 +0300

    Logo change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
     <div>
         <nav class="navbar navbar-expand-lg navbar-light">
             <a class="navbar-brand" href="{{ url("/") }}">
-                <img src="{{ asset('img/edupartnerlogo4.png') }}">
+                <img src="{{ asset('img/Education-For-White-BG.png') }}">
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -60,7 +60,7 @@
         <div class="row">
             <div class="col-sm-6">
                 <a class="navbar-brand" href="#">
-                    <img src="{{ asset('img/edupartnerlogo5.png') }}">
+                    <img src="{{ asset('img/Education-For-Grey-BG.png') }}">
                 </a>
                 <p class="white">Eduvation is a regional educational management firm specialized in designing new operating models for educational institutes. The purpose of such a customized operating model is to help schools convert strategies to tangible results.
                 </p>
